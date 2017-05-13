7cf53a5cd9109dd14cff986c0cb3662da2169ddf
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-13 22:17:48 +0200

    Renames `len` within the context of `function`.

--- function.js
+++ function.js
@@ -1,17 +1,20 @@
 const { iif } = require('./logic.js');
 const { len } = require('./list.js');
 
+const arity = len;
+
 const compose = (...fns) => x => fns.reduceRight((v, fn) => fn(v), x);
 
 const apply = fn => (...args) => fn.bind(null, ...args);
 
 const call = fn => fn.call(null);
 
-const curry = fn => (...args) => compose(iif(len)(curry)(call), apply(fn))(...args);
+const curry = fn => (...args) => compose(iif(arity)(curry)(call), apply(fn))(...args);
 
 const flip = fn => b => a => fn(a, b);
 
 module.exports = {
+    arity,
     compose,
     apply,
     call,
