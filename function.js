da3d44a5bb41908b32816f0544eda888774b32f4
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-13 14:29:39 +0200

    Refactors `function.curry`.

--- function.js
+++ function.js
@@ -3,14 +3,17 @@ const { len } = require('./list.js');
 
 const compose = (...fns) => x => fns.reduceRight((v, fn) => fn(v), x);
 
+const apply = fn => (...args) => fn.bind(null, ...args);
+
 const call = fn => fn.call(null);
 
-const curry = fn => (...args) => iif(len)(curry)(call)(fn.bind(null, ...args));
+const curry = fn => (...args) => compose(iif(len)(curry)(call), apply(fn))(...args);
 
 const flip = fn => b => a => fn(a, b);
 
 module.exports = {
     compose,
+    apply,
     call,
     curry,
     flip
