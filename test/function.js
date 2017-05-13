11c26d24b197df5d115a6b30872d212bb9dd5e16
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-13 23:23:18 +0200

    Adds some `function` tests.

--- test/function.js
+++ test/function.js
@@ -1,10 +1,21 @@
 const test = require('tape');
 const sinon = require('sinon');
 
+const { ftrue } = require('../logic.js');
 // functions to test
 const { arity, compose, apply, call, curry, flip } = require('../function.js');
 
 test('arity', t => {
+    const f0 = () => void 0;
+    const f1 = (a) => void 0;
+    const f2 = (a, b) => void 0;
+    const f3 = (a, b, c) => void 0;
+
+    t.equal(0, arity(f0));
+    t.equal(1, arity(f1));
+    t.equal(2, arity(f2));
+    t.equal(3, arity(f3));
+
     t.end();
 });
 
@@ -17,6 +28,12 @@ test('apply', t => {
 });
 
 test('call', t => {
+    const f = sinon.spy(ftrue);
+
+    t.true(call(f));
+    t.true(f.calledOnce);
+    t.true(f.calledWith());
+
     t.end();
 });
 
@@ -25,5 +42,16 @@ test('curry', t => {
 });
 
 test('flip', t => {
+    const fi = sinon.spy(ftrue);
+    const fo = sinon.spy(_ => fi);
+
+    const f = flip(fo);
+
+    t.true(f(true)(false));
+    t.true(fo.calledOnce);
+    t.true(fo.calledWith(false));
+    t.true(fi.calledOnce);
+    t.true(fi.calledWith(true));
+
     t.end();
 });
