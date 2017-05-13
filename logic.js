09bc6078a46ab3db4ce3336b32dd1c5a545530a6
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-13 17:08:34 +0200

    Adds more `logic` functions.

--- logic.js
+++ logic.js
@@ -1,11 +1,26 @@
+const id = x => x;
+
+const ftrue = _ => true;
+
+const ffalse = _ => false;
+
 const iif = pr => t => f => x => pr(x) ? t(x) : f(x);
 
 const not = fn => x => !fn(x);
 
 const and = pra => prb => x => pra(x) && prb(x);
 
+const or = pra => prb => x => pra(x) || prb(x);
+
+const xor = pra => prb => and(or(pra)(prb))(not(and(pra)(prb)));
+
 module.exports = {
+    id,
+    ftrue,
+    ffalse,
     iif,
     not,
-    and
+    and,
+    or,
+    xor
 };
