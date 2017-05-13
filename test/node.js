94b056c0d07154ea14e32fc226aec4e3b4074a03
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-14 00:05:27 +0200

    Adds `node` tests.

--- test/node.js
+++ test/node.js
@@ -5,5 +5,7 @@ const sinon = require('sinon');
 const { exportAll } = require('../node.js');
 
 test('exportAll', t => {
+    t.deepEqual({a: 1, b: 2, c: 3}, exportAll(['./test/mock/a.js', './test/mock/b.js', './test/mock/c.js']));
+
     t.end();
 });
